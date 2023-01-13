b6fc1292b8825c16a32d21bee9f36c84824a06e0
Dawa <sherpadawa01@outlook.com>
2023-01-13 21:42:05 +0545

    navbar completed

---
+++ youtube_clone/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Watch from './pages/Watch';
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/watch/:id" element={<Watch />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
